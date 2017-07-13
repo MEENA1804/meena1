a8cb9abb669817e943c3b43438e9b4710fceaec1
MEENA1804 <meenanandy111@gmail.com>
2017-07-13 16:09:48 +0530

    Add files via upload

---
+++ Reversal.java
@@ -0,0 +1,20 @@
+package meenu;
+import java.util.*;
+public class Reversal {
+
+	public static void main(String[] args) 
+	{
+	int no,rev=0,r,a;
+	Scanner s=new Scanner(System.in);
+	System.out.println("Enter any no.: ");
+	no=s.nextInt();
+	a=no;
+	while(no>0)
+	{
+	r=no%10;
+	rev=rev*10+r;
+	no=no/10;
+	}
+	System.out.println("Reverse: "+rev);
+	}
+	}
